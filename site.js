d62bf9d509ab70080e29e95935ac6b083dd5e000
natchga <pp21800@gmail.com>
2025-10-04 11:36:47 -0500

    Added carousel
    Assignment 5

--- site.js
+++ site.js
@@ -19,4 +19,45 @@ window.addEventListener("DOMContentLoaded", () => {
   welcomeDiv.textContent = message;
 
   localStorage.setItem("It's a secret to everybody.", "the cake is a lie, easy portal reference, give me the brownie points");
+
+//Carousel below
+  const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  ].map(url => { (new Image()).src = url; return url });
+
+  const images = document.querySelectorAll('#carousel img');
+  let currentImage = 0;
+
+  const showImages = () => {
+    const offset = currentImage % urls.length;
+    images.forEach((image, index) => {
+      const imageIndex = (index + offset + urls.length) % urls.length;
+      image.src = urls[imageIndex];
+    });
+  };
+
+  showImages();
+
+  const prevBtn = document.getElementById('prev');
+  const nextBtn = document.getElementById('next');
+
+  prevBtn.addEventListener('click', () => {
+    currentImage = (currentImage - 1 + urls.length) % urls.length;
+    showImages();
+  });
+
+  nextBtn.addEventListener('click', () => {
+    currentImage = (currentImage + 1) % urls.length;
+    showImages();
+  });
+
+  setInterval(() => {
+    // code to run EVERY 5 seconds
+    currentImage = (currentImage + 1) % urls.length;
+    showImages();
+  }, 5000);
 });
